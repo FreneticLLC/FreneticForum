1f7270ad9c29699d1923ecafe0e50eda5b016514
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-04-07 07:41:41 -0700

    update to latest dnc

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace FreneticForum
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             builder.AddEnvironmentVariables();
