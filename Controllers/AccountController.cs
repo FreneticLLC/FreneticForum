e2db8dffe9cd2252ce70f49d1d9c10f442b3ecfd
mcmonkey4eva <agoodwin@freneticllc.com>
2017-10-16 20:14:41 -0700

    API Notes, prep

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -54,9 +54,51 @@ namespace FreneticForum.Controllers
         {
             try
             {
-                ViewData["init"] = new ForumInit(Request, Response);
-                ViewData["is_post"] = Request.Method.ToUpperInvariant() == "POST";
-                return View();
+                ForumInit finit = new ForumInit(Request, Response);
+                if (Request.Method.ToUpperInvariant() != "POST")
+                {
+                    return Content("ERROR=NOT_POST;");
+                }
+                String qtype = Request.Form["qtype"];
+                if (qtype == null)
+                {
+                    return Content("ERROR=NO_QTYPE;");
+                }
+                qtype = qtype.ToLowerInvariant();
+                // Log in to an account
+                if (qtype == "login")
+                {
+                    // Username, Password, TFA
+                    return Content("ERROR=NOT_IMPLEMENTED;");
+                }
+                // Log out of an account
+                else if (qtype == "logout")
+                {
+                    // Username, Session
+                    return Content("ERROR=NOT_IMPLEMENTED;");
+                }
+                // Gather a one-use key to log in to a server.
+                else if (qtype == "one_use_key")
+                {
+                    // Username, Session, KeyTypeID
+                    return Content("ERROR=NOT_IMPLEMENTED;");
+                }
+                // A server wants to check a one-use key for validity.
+                else if (qtype == "check_key")
+                {
+                    // Username, Key, KeyTypeID
+                    return Content("ERROR=NOT_IMPLEMENTED;");
+                }
+                // Gather a bit of information on a user's public profile data, if available.
+                else if (qtype == "mini_profile")
+                {
+                    // Username
+                    return Content("ERROR=NOT_IMPLEMENTED;");
+                }
+                else
+                {
+                    return Content("ERROR=UNKNOWN;");
+                }
             }
             catch (NoProcessException)
             {
