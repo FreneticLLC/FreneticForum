3372b4205f301ef9152c0f3f442c2c09e7216853
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2025-08-17 23:33:02 -0700

    that url is wonk

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gulp clean min
 dotnet restore
-ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=http://*:8050 dotnet run
+ASPNETCORE_ENVIRONMENT=Production ASPNETCORE_URLS=http://0.0.0.0:8050 dotnet run
